f90985e9b82abc51e769be99636a38f297c90584
Igor <156268020+igordev28@users.noreply.github.com>
2024-09-03 21:15:53 -0300

    exe1

--- exe2/Classificacao.php
+++ exe2/Classificacao.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Classificação de Notas</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form method="post" action="">
